2672110ba1ef90fd4d9939abd836b6442318638e
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2025-12-28 15:15:59 +0700

    Continue to do navigation ...

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -72,3 +72,70 @@ const paginationSlider = new Swiper('.slider-pagination', {
         },
     },
 })
+
+const parallaxSwiper = new Swiper('.parallax-grid', {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    loop: true,
+    speed: 600,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    allowTouchMove: true,
+    // grabCursor: true,
+
+    pagination: {
+        el: '.parallax-pagination',
+        clickable: true,
+    },
+
+    breakpoints: {
+        0: {
+
+        },
+        576: {
+
+        },
+        768: {
+
+        },
+        992: {
+
+        },
+        1200: {
+            slidesPerView: 3,
+        },
+        1400: {
+            slidesPerView: 3,
+        },
+    },
+})
+
+const swiper = new Swiper(".swiper-banner", {
+    // slidesPerView: 1,
+    loop: true,
+    speed: 1200,
+    effect: "fade",
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    grabCursor: false,
+    allowTouchMove: false,
+});
+
+swiper.on("slideChangeTransitionStart", () => {
+    document
+        .querySelectorAll(".swiper-slide .content > *")
+        .forEach(el => {
+            el.style.animation = "none";
+            el.offsetHeight; // force reflow
+            el.style.animation = "";
+        });
+});
