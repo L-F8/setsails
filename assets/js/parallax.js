eea69fd14cef457eab3a1631237eebe33cdc2c54
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2026-01-03 16:12:28 +0700

    Countinue finishing pages for submenu link

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -2,5 +2,5 @@ const bg = document.getElementById('bg');
 
 window.addEventListener('scroll', function () {
     let offset = window.pageYOffset;
-    bg.style.transform = `translateY(${offset * 0.5}px)`;
+    bg.style.transform = `translateY(${offset * 0.35}px)`;
 });
